5f149999d6a53f9c7e6b2d847f444435223b4439
Maksim <ddyamaks@gmail.com>
2022-09-10 21:28:45 +0300

    planet coulun

---
+++ seminar 1/planet_coulun.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<char> hexidecimal = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    
+    string name;
+    cout << "Product's name: ";
+    cin >> name;
+
+
+    int price;
+    cout << "Product's price: ";
+    cin >> price;
+    string s = "";
+    while (price > 0) {
+        s = hexidecimal[price % 16] + s;
+        price = price / 16;
+    } // далее вместо "price" работаем с "s"
+
+    string true_false;
+    cout << "Is cash-back available for this product? (true/false) ";
+    cin >> true_false;
+
+    float temperature;
+    cout << "Maximum storing temperature: ";
+    cin >> temperature;
+
+    cout << '\n' << name;
+    cout << "\nPrice:..........." << s;
+    cout << "\nHas cash-back:......." << true_false;
+
+    cout << "\nMax temperature:......";
+    if (temperature > 0) {
+        cout << "+";
+    }
+    cout << temperature;
+    return 0;
+}
